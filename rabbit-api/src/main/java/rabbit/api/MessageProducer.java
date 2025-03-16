48e614e0ef8a5ee7f57219a26a6f09b6f156bc02
kenny <kenny.kwyiu@gmail.com>
2025-03-16 11:30:03 +0800

    MessageProducer

--- rabbit-api/src/main/java/rabbit/api/MessageProducer.java
+++ rabbit-api/src/main/java/rabbit/api/MessageProducer.java
@@ -4,9 +4,38 @@ import rabbit.api.exception.MessageRunTimeException;
 
 import java.util.List;
 
+/**
+ * MessageProducer is an abstraction of the RabbitMQ producer API.
+ * It provides methods for sending messages to a message broker.
+ */
 public interface MessageProducer {
-    void send(Message message, SendCallback sendCallback) throws MessageRunTimeException;
-    void send(Message message) throws MessageRunTimeException;
-    void send(List<Message> messages) throws MessageRunTimeException;
 
+    /**
+     * Sends a message asynchronously with a callback.
+     * The callback provides confirmation of message delivery.
+     *
+     * @param message      The message to be sent.
+     * @param sendCallback The callback to handle success or failure events.
+     * @throws MessageRunTimeException if an error occurs during message sending.
+     */
+    void send(Message message, SendCallback sendCallback) throws MessageRunTimeException;
+
+    /**
+     * Sends a message synchronously without a callback.
+     * This method is typically used when message confirmation is not required.
+     *
+     * @param message The message to be sent.
+     * @throws MessageRunTimeException if an error occurs during message sending.
+     */
+    void send(Message message) throws MessageRunTimeException;
+
+    /**
+     * Sends a batch of messages in a single operation.
+     * Useful for improving performance when sending multiple messages at once.
+     *
+     * @param messages The list of messages to be sent.
+     * @throws MessageRunTimeException if an error occurs during message sending.
+     */
+    void send(List<Message> messages) throws MessageRunTimeException;
 }
+
