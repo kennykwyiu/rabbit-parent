b5c8bdd4c633361dac8b74700a1f7b27a354b05e
kenny <kenny.kwyiu@gmail.com>
2025-03-16 11:19:26 +0800

    MessageException

---
+++ rabbit-api/src/main/java/rabbit/api/exception/MessageException.java
@@ -0,0 +1,21 @@
+package rabbit.api.exception;
+
+public class MessageException extends Exception {
+    private static final long serialVersionUID = -1896774862170691027L;
+
+    public MessageException() {
+        super();
+    }
+
+    public MessageException(String message) {
+        super(message);
+    }
+
+    public MessageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MessageException(Throwable cause) {
+        super(cause);
+    }
+}
