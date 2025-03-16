e50babdc2d1f91f0cb3fdb6422be651499878bc9
kenny <kenny.kwyiu@gmail.com>
2025-03-16 20:03:37 +0800

    MessageListener

--- rabbit-api/src/main/java/rabbit/api/MessageListener.java
+++ rabbit-api/src/main/java/rabbit/api/MessageListener.java
@@ -1,5 +1,17 @@
 package rabbit.api;
 
+/**
+ * MessageListener is a callback interface for handling incoming RabbitMQ messages.
+ * Implementations of this interface should define logic to process received messages.
+ */
 public interface MessageListener {
+
+    /**
+     * Called when a new message is received.
+     * Implementations should handle message processing, such as parsing or persisting data.
+     *
+     * @param message the received message
+     */
     void onMessage(Message message);
 }
+
