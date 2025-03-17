acdd05f9cb5ce9dfb86b088535878d6f9d71b2db
kenny <kenny.kwyiu@gmail.com>
2025-03-17 20:45:00 +0800

    RabbitProducerAutoConfiguration

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/autoconfigure/RabbitProducerAutoConfiguration.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/autoconfigure/RabbitProducerAutoConfiguration.java
@@ -1,7 +1,9 @@
 package com.kenny.rabbit.producer.autoconfigure;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan({"com.kenny.rabbit.producer.*"})
 public class RabbitProducerAutoConfiguration {
 }
