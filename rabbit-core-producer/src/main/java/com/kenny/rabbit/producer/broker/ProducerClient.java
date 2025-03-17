421014148d002cab7f34a9b7516554c24b5d0650
kenny <kenny.kwyiu@gmail.com>
2025-03-17 20:46:01 +0800

    ProducerClient

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/ProducerClient.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/ProducerClient.java
@@ -1,16 +1,38 @@
 package com.kenny.rabbit.producer.broker;
 
+import com.google.common.base.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import rabbit.api.Message;
 import rabbit.api.MessageProducer;
+import rabbit.api.MessageType;
 import rabbit.api.SendCallback;
 import rabbit.api.exception.MessageRunTimeException;
 
 import java.util.List;
 
+@Component
 public class ProducerClient implements MessageProducer {
+    @Autowired
+    private RabbitBroker rabbitBroker;
+
     @Override
     public void send(Message message, SendCallback sendCallback) throws MessageRunTimeException {
-
+        Preconditions.checkNotNull(message.getTopic());
+        MessageType messageType = message.getMessageType();
+        switch (messageType) {
+            case RAPID:
+                rabbitBroker.rapidSend(message);
+                break;
+            case CONFIRM:
+                rabbitBroker.confirmSend(message);
+                break;
+            case RELIANT:
+                rabbitBroker.reliantSend(message);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
