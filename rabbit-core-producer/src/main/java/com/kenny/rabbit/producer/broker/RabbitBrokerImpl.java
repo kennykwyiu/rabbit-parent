7af6cf92e9888abaa489fa20c3226f78cb86dcc2
kenny <kenny.kwyiu@gmail.com>
2025-03-18 23:19:55 +0800

    rapidSend

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitBrokerImpl.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitBrokerImpl.java
@@ -1,11 +1,24 @@
 package com.kenny.rabbit.producer.broker;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rabbit.api.Message;
+import rabbit.api.MessageType;
+
 @Component
-public class RabbitBrokerImpl implements RabbitBroker{
+public class RabbitBrokerImpl implements RabbitBroker {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public void rapidSend(Message message) {
+        message.setMessageType(MessageType.RAPID);
+        sendKernel(message);
+    }
+
+    private void sendKernel(Message message) {
 
     }
 
