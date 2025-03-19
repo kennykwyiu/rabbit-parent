c6d20643dd192e67a7c422de3cb2bfe1c1b3e3d8
kenny <kenny.kwyiu@gmail.com>
2025-03-19 21:12:21 +0800

    sendKernel

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitBrokerImpl.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitBrokerImpl.java
@@ -1,11 +1,13 @@
 package com.kenny.rabbit.producer.broker;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import rabbit.api.Message;
 import rabbit.api.MessageType;
-
+@Slf4j
 @Component
 public class RabbitBrokerImpl implements RabbitBroker {
 
@@ -19,7 +21,13 @@ public class RabbitBrokerImpl implements RabbitBroker {
     }
 
     private void sendKernel(Message message) {
-
+        CorrelationData correlationData = new CorrelationData(String.format("%s#%s",
+                                                        message.getMessageId(),
+                                                        System.currentTimeMillis()));
+        String topic = message.getTopic();
+        String routingKey = rabbitTemplate.getRoutingKey();
+        rabbitTemplate.convertAndSend(topic, routingKey, message, correlationData);
+        log.info("#RabbitBrokerImpl.sendKernel# send to rabbitmq, messageId: {}", message.getMessageId());
     }
 
     @Override
