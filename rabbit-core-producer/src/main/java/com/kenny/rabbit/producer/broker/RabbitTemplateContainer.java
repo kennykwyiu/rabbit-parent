e15c5e5bedb3560e3d2530920e4d077ef187ffd8
kenny <kenny.kwyiu@gmail.com>
2025-03-22 14:57:33 +0800

    RabbitTemplateContainer - getClassAndMethodName

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitTemplateContainer.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/broker/RabbitTemplateContainer.java
@@ -1,12 +1,17 @@
 package com.kenny.rabbit.producer.broker;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
+import rabbit.api.Message;
+import rabbit.api.MessageType;
+import rabbit.api.exception.MessageException;
 
 import java.util.Map;
 
@@ -18,6 +23,12 @@ public class RabbitTemplateContainer implements RabbitTemplate.ConfirmCallback {
 
     @Autowired
     private ConnectionFactory connectionFactory;
+
+    private String getClassAndMethodName() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        StackTraceElement element = stackTrace[2]; // index 2 gives the caller method
+        return "#" + element.getClassName() + "." + element.getMethodName() + "#";
+    }
 
     @Override
     public void confirm(CorrelationData correlationData, boolean ack, String cause) {
