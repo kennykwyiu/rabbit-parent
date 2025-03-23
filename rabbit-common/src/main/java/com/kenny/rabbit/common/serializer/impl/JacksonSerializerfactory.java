bb3df260c656599121c5746c02bb51889400295e
kenny <kenny.kwyiu@gmail.com>
2025-03-23 11:48:49 +0800

    JacksonSerializerfactory

---
+++ rabbit-common/src/main/java/com/kenny/rabbit/common/serializer/impl/JacksonSerializerfactory.java
@@ -0,0 +1,11 @@
+package com.kenny.rabbit.common.serializer.impl;
+
+import com.kenny.rabbit.common.serializer.Serializer;
+import com.kenny.rabbit.common.serializer.Serializerfactory;
+
+public class JacksonSerializerfactory implements Serializerfactory {
+    @Override
+    public Serializer create() {
+        return null;
+    }
+}
