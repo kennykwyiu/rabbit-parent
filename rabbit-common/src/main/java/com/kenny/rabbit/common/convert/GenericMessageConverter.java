9b82218d411f36250546c6ea2978237ec3f75e7b
kenny <kenny.kwyiu@gmail.com>
2025-03-23 12:23:32 +0800

    add serializer in GenericMessageConverter

--- rabbit-common/src/main/java/com/kenny/rabbit/common/convert/GenericMessageConverter.java
+++ rabbit-common/src/main/java/com/kenny/rabbit/common/convert/GenericMessageConverter.java
@@ -1,10 +1,19 @@
 package com.kenny.rabbit.common.convert;
 
+import com.google.common.base.Preconditions;
+import com.kenny.rabbit.common.Serializer;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.converter.MessageConverter;
 
 public class GenericMessageConverter implements MessageConverter {
+    private Serializer serializer;
+
+    public GenericMessageConverter(Serializer serializer) {
+        Preconditions.checkNotNull(serializer);
+        this.serializer = serializer;
+    }
+
     @Override
     public Object fromMessage(Message<?> message, Class<?> aClass) {
         return null;
