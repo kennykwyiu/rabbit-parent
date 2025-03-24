89b7aa4f38c6e7c8bb6f821b61810f0ed43e880a
kenny <kenny.kwyiu@gmail.com>
2025-03-24 14:45:01 +0800

    constructor

--- rabbit-common/src/main/java/com/kenny/rabbit/common/convert/RabbitMessageConverter.java
+++ rabbit-common/src/main/java/com/kenny/rabbit/common/convert/RabbitMessageConverter.java
@@ -1,10 +1,18 @@
 package com.kenny.rabbit.common.convert;
 
+import com.google.common.base.Preconditions;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.converter.MessageConverter;
 
 public class RabbitMessageConverter implements MessageConverter {
+    private GenericMessageConverter delegate;
+
+    public RabbitMessageConverter(GenericMessageConverter genericMessageConverter) {
+        Preconditions.checkNotNull(genericMessageConverter);
+        this.delegate = genericMessageConverter;
+    }
+
     @Override
     public Object fromMessage(Message<?> message, Class<?> aClass) {
         return null;
