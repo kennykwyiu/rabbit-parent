3f4177a6badfac2dcba184872664e18b0d581a3d
kenny <kenny.kwyiu@gmail.com>
2025-03-24 14:42:53 +0800

    RabbitMessageConverter

---
+++ rabbit-common/src/main/java/com/kenny/rabbit/common/convert/RabbitMessageConverter.java
@@ -0,0 +1,17 @@
+package com.kenny.rabbit.common.convert;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.converter.MessageConverter;
+
+public class RabbitMessageConverter implements MessageConverter {
+    @Override
+    public Object fromMessage(Message<?> message, Class<?> aClass) {
+        return null;
+    }
+
+    @Override
+    public Message<?> toMessage(Object o, MessageHeaders messageHeaders) {
+        return null;
+    }
+}
