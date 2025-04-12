576bc9d426066b86bce7a18e5bfe9b0315f110f1
kenny <kenny.kwyiu@gmail.com>
2025-04-12 21:02:40 +0800

    BrokerMessageStatus

---
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/constant/BrokerMessageStatus.java
@@ -0,0 +1,15 @@
+package com.kenny.rabbit.producer.constant;
+
+public enum BrokerMessageStatus {
+    SENDING("0"), SEND_OK("1"), SEND_FAIL("2");
+    private String code;
+
+    private BrokerMessageStatus(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+}
