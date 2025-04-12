2534e9920d2d48f44b13649fa116db528f272ea0
kenny <kenny.kwyiu@gmail.com>
2025-04-12 16:54:01 +0800

    MessageStoreServiceImpl

---
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/service/MessageStoreServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kenny.rabbit.producer.service;
+
+import com.kenny.rabbit.producer.entity.BrokerMessage;
+import com.kenny.rabbit.producer.mapper.BrokerMessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageStoreServiceImpl implements MessageStoreService {
+
+}
