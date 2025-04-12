74f8f969aa5786e6c00457848a1a730c1caf0e20
kenny <kenny.kwyiu@gmail.com>
2025-04-12 21:01:40 +0800

    success

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/service/MessageStoreServiceImpl.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/service/MessageStoreServiceImpl.java
@@ -1,11 +1,28 @@
 package com.kenny.rabbit.producer.service;
 
+import com.kenny.rabbit.producer.constant.BrokerMessageStatus;
 import com.kenny.rabbit.producer.entity.BrokerMessage;
 import com.kenny.rabbit.producer.mapper.BrokerMessageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class MessageStoreServiceImpl implements MessageStoreService {
+    @Autowired
+    private BrokerMessageMapper brokerMessageMapper;
+
+    @Override
+    public int insert(BrokerMessage brokerMessage) {
+        return this.brokerMessageMapper.insert(brokerMessage);
+    }
+
+    @Override
+    public void success(String messageId) {
+        this.brokerMessageMapper.changeBrokerMessageStatus(messageId,
+                                                            BrokerMessageStatus.SEND_OK.getCode(),
+                                                            new Date());
+    }
 
 }
