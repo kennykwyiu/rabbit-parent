8e5de4a39b7616e3270ed8280aeccb97b914dd14
kenny <kenny.kwyiu@gmail.com>
2025-04-13 20:23:33 +0800

    add SEND_FAIL_A_MOMENT

--- rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/constant/BrokerMessageStatus.java
+++ rabbit-core-producer/src/main/java/com/kenny/rabbit/producer/constant/BrokerMessageStatus.java
@@ -1,7 +1,10 @@
 package com.kenny.rabbit.producer.constant;
 
 public enum BrokerMessageStatus {
-    SENDING("0"), SEND_OK("1"), SEND_FAIL("2");
+    SENDING("0"),
+    SEND_OK("1"),
+    SEND_FAIL("2"),
+    SEND_FAIL_A_MOMENT("3");
     private String code;
 
     private BrokerMessageStatus(String code) {
